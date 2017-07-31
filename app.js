30a06df7dbecb72dcd790ae25f804fd47fabb03b
Anna Chen <xqchen91@gmail.com>
2017-07-31 18:28:12 -0400

    progress so far

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
+app.use(function (request, response, next) {
+    // do your logging here
+    console.log(request.method+' '+ request.originalUrl);
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    next();
+})
 
 app.get('/',function(request,response){
     response.send('Hello World!');
 })
 
 app.listen(3000,function(){
-console.log('listening on 3000')
+    console.log('listening on 3000')
 })
