989d562bf9683456c51234c566a916a14b10a590
Kimberly Ton <kimberlyton@gmail.com>
2017-08-01 15:11:05 -0400

    after lunch

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,19 @@ router.get('/', function (req, res) {
   res.render( 'index', { tweets: tweets } );
 });
 
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name.split(" ");
+  console.log(name)
+  name = name.map(function (str){
+    return str.charAt(0).toUpperCase()+str.slice(1);
+    }).join(" ")
+  console.log(name)
+  console.log("I am here")
+  var list = tweetBank.find( {name: name } );
+  console.log(list)
+//   console.log(name)
+//   console.log(tweetBank.list())
+  res.render( 'index', { tweets: list } );
+});
 
 module.exports = router;
--- tweetBank.js
+++ tweetBank.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 var data = [];
 
 function add (name, content) {
-  data.push({ name: name, content: content });
+  data.push({ name: name, content: content, id: });
 }
 
 function list () {
@@ -31,9 +31,14 @@ const getFakeTweet = function() {
   return "Fullstack Academy is awesome" + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
 };
 
+const createID = function(){
+  
+}
+
 for (let i = 0; i < 10; i++) {
   module.exports.add( getFakeName(), getFakeTweet() );
 }
+module.exports.add("Karen", "sample tweet")
 
 //console.log(data)
 console.log(find('Karen'))
